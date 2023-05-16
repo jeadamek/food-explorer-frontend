82c2af545cafac679fa0d3139ddd0927787de9db
jessica <jessica.g.adamek@gmail.com>
2023-05-16 20:06:39 -0300

    :sparkles: Sign in page created

--- src/main.jsx
+++ src/main.jsx
@@ -10,12 +10,13 @@ import { AddDish } from './pages/AddDish'
 import { EditDish } from './pages/EditDish'
 import { HomeUser } from './pages/HomeUser'
 import { SignUp } from './pages/SignUp'
+import { SignIn } from './pages/SignIn'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignUp />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,47 @@
+import { Container, Form } from "./styles";
+
+import { Label } from "../../components/Label";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+// import { Link } from 'react-router-dom';
+
+import Logo from "../../assets/logo-user.svg"
+
+export function SignIn() {
+  return(
+    <Container>
+      <img src={Logo} alt="Logo Food Explorer" />
+
+      <Form>
+        <h2>Faça login</h2>
+
+        <div className="input-wrapper">
+          <Label title="Email" htmlFor="email" />
+          <Input
+            type="email"
+            placeholder="Exemplo: exemplo@exemplo.com.br"
+            id="email"
+            name="email"
+          />
+        </div>
+
+        <div className="input-wrapper">
+          <Label title="Senha" htmlFor="password" />
+          <Input
+            type="password"
+            placeholder="No mínimo 6 caracteres"
+            id="password"
+            name="password"
+          />
+        </div>
+
+        <Button title="Entrar" className="primary" />
+
+        <a href="/register">
+          Criar uma conta
+        </a>
+      </Form>
+    </Container>
+  )
+}

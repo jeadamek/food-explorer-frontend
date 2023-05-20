f308b2059e4372b007bc82511aa6a12b5dcfa954
jessica <jessica.g.adamek@gmail.com>
2023-05-20 13:38:00 -0300

    :green_heart: access to add dish and edit dish by only admin

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,17 +1,22 @@
 import { Routes, Route } from 'react-router-dom';
 
+import { useAuth } from '../hooks/auth';
+
 import { Home } from '../pages/Home';
 import { EditDish } from '../pages/EditDish';
 import { AddDish } from '../pages/AddDish';
 import { DishDetails } from '../pages/DishDetails';
 
 export function AppRoutes() {
+
+  const { user } = useAuth();
   return(
     <Routes>
-      {/* TODO: Separar rotas admin e cliente */}
+      {/* ROUTES ACCESS JUST BY ADMIN  */}
+      {user.isAdmin &&  <Route path="/new" element={<AddDish />} />}
+      {user.isAdmin &&  <Route path="/edit/:id" element={<EditDish />} />}
+
       <Route path="/" element={<Home />} />
-      <Route path="/new" element={<AddDish />} />
-      <Route path="/edit/:id" element={<EditDish />} />
       <Route path="/details/:id" element={<DishDetails />} />
     </Routes>
   )

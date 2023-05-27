b11cdd6c6aaae0437da1c70474a79b6f312a191c
jessica <jessica.g.adamek@gmail.com>
2023-05-26 23:21:49 -0300

    :hammer: add function to pencil button

--- src/components/CardAdmin/index.jsx
+++ src/components/CardAdmin/index.jsx
@@ -1,6 +1,7 @@
+import { useNavigate } from "react-router-dom";
+import PropTypes from "prop-types";
 import { Container } from "./styles";
 
-import PropTypes from "prop-types";
 import { api } from "../../services/api";
 
 import { TbPencil } from "react-icons/tb";
@@ -9,10 +10,17 @@ export function CardAdmin({ dish, ...rest }) {
   const priceInCurrency = dish.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
 
   const imageUrl = `${api.defaults.baseURL}/files/${dish.image}`
+  
+  const navigate = useNavigate();
+
+  function handleEdit(event) {
+    event.preventDefault();
+    navigate(`/edit/${dish.id}`);
+  }
 
   return(
     <Container {...rest}>
-      <TbPencil size={28} />
+      <TbPencil size={28} onClick={handleEdit}/>
       <img src={imageUrl} alt={`imagem de ${dish.name}`}/>
       <h3>{`${dish.name} >`}</h3>
       <p>{dish.description}</p>

7ce6ee78b31db996b0466761a854264529477e9d
jessica <jessica.g.adamek@gmail.com>
2023-05-28 16:07:03 -0300

    :sparkles: create favorite feature to CardUser

--- src/components/CardUser/index.jsx
+++ src/components/CardUser/index.jsx
@@ -9,23 +9,48 @@ import { Stepper } from "../Stepper";
 
 import { IoMdHeartEmpty } from 'react-icons/io'
 import { IoMdHeart } from 'react-icons/io'
+import { useState } from "react";
+import { useEffect } from "react";
 
 export function CardUser({ dish, ...rest }) {
+  const [isFavorite, setIsFavorite] = useState();
+  const [idFavorite, setIdFavorite] = useState();
   const priceInCurrency = dish.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
 
   const imageUrl = `${api.defaults.baseURL}/files/${dish.image}`
   
   async function handleFavorite(event) {
     event.preventDefault();
-    const dish_id = dish.id;
-
-    await api.post("/favorites/", { dish_id });
-    alert("adicionado!")
+    if (isFavorite) {
+      await api.delete(`/favorites/${idFavorite}`);
+      setIsFavorite(false);
+    } else {
+      const id = await api.post("/favorites/", { dish_id: dish.id });
+      setIsFavorite(true);
+      setIdFavorite(id);
+    }
   }
+
+  useEffect(() => {
+    async function fetchFavorite() {
+      const response = await api.get('/favorites');
+      
+      const favorite = response.data.find(
+        favorite => favorite.dish_id === dish.id
+      )
+      
+      setIsFavorite(favorite ? true : false);
+      setIdFavorite(favorite ? favorite.id : null);
+    }
+
+    fetchFavorite();
+  }, [dish.id]);
 
   return(
     <Container {...rest}>
-      <button onClick={handleFavorite}>{dish.favorite ? <IoMdHeart size={28} /> : <IoMdHeartEmpty size={28} />}</button>
+      <button onClick={handleFavorite}>
+        {isFavorite ? <IoMdHeart size={28} /> : <IoMdHeartEmpty size={28} />}
+      </button>
       <img src={imageUrl} alt={`imagem de ${dish.name}`}/>
       <h3>{`${dish.name}`}</h3>
       <p>{dish.description}</p>
@@ -39,5 +64,5 @@ export function CardUser({ dish, ...rest }) {
 }
 
 CardUser.propTypes = {
-  dish: PropTypes.object.isRequired
+  dish: PropTypes.object.isRequired,
 }

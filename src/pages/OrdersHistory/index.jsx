c6ddf676cb9392201e97340d8a830a654fccd4f5
jessica <jessica.g.adamek@gmail.com>
2023-05-28 22:05:21 -0300

    :construction: build OrdersHistory responsive layout

--- src/pages/OrdersHistory/index.jsx
+++ src/pages/OrdersHistory/index.jsx
@@ -1,4 +1,4 @@
-import { Container } from "./styles";
+import { Container, ContentMobile, ContentDesktop } from "./styles";
 
 import { Header } from "../../components/Header";
 import { Footer } from "../../components/Footer";
@@ -9,7 +9,45 @@ export function OrdersHistory() {
       <Header />
 
       <main>
-        <h1>Pedidos</h1>
+        <h1>Histórico de pedidos</h1>
+        <ContentMobile>
+          <span className="code">000004</span>
+          <span className="status">Pendente</span>
+          <span className="time">20/05 às 18h00</span>
+          <p className="details">
+            1 x Salada Radish, 1 x Torradas de Parma, 1 x Chá de Canela, 1 x Suco de Maracujá
+          </p>
+        </ContentMobile>
+
+        <ContentDesktop>
+          <thead>
+            <tr>
+              <th>Status</th>
+              <th>Código</th>
+              <th>Detalhamento</th>
+              <th>Data e hora</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Pendente</td>
+              <td>00000004</td>
+              <td>1 x Salada Radish, 1 x Torradas de Parma, 1 x Chá de Canela, 1 x Suco de Maracujá</td>
+              <td>20/05 às 18h00</td>
+            </tr>
+            <tr>
+              <td>Pendente</td>
+              <td>00000004</td>
+              <td>1 x Salada Radish, 1 x Torradas de Parma, 1 x Chá de Canela, 1 x Suco de Maracujá</td>
+              <td>20/05 às 18h00</td>
+            </tr>
+          </tbody>
+        </ContentDesktop>
+
+
+
+
+
       </main>
 
       <Footer />
--- src/pages/OrdersHistory/styles.js
+++ src/pages/OrdersHistory/styles.js
@@ -14,11 +14,20 @@ export const Container = styled.div`
 
   > main {
     grid-area: content;
-    padding: 4rem 2.8rem;
-
     width: 100%;
 
+    padding: 4rem 2.8rem;
     margin: 0 auto;
+
+    font-family: 'Roboto', sans-serif;
+    font-size: 1.4rem;
+    line-height: 160%;
+
+    color: ${({ theme }) => theme.COLORS.LIGHT_400};
+
+    > h1 {
+      margin-bottom: 2.8rem;
+    }
   }
 
   @media ${device.laptop} {
@@ -26,6 +35,11 @@ export const Container = styled.div`
 
     > main {
       padding: 3.4rem 8rem;
+
+      > h1 {
+        margin-bottom: 3.8rem;
+        font-size: 3.2rem;
+      }
     }
   }
 
@@ -33,5 +47,119 @@ export const Container = styled.div`
     > main {
       padding: 3.4rem 12.3rem;
     }
+  }
+`;
+
+export const ContentMobile = styled.div`
+  width: 100%;
+  
+  background-color: transparent;
+  border: solid 2px ${({ theme }) => theme.COLORS.DARK_1000};
+
+  display: grid;
+  grid-template-columns: 0.5fr 1fr 1fr;
+  grid-template-areas: 
+    "code status time"
+    "details details details";
+
+  align-items: center;
+  gap: 1.6rem;
+
+  padding: 1.6rem 2rem;
+
+  border-radius: .8rem;
+
+  .code {
+    grid-area: code;
+  }
+
+  .status {
+    grid-area: status;
+    justify-self: center;
+
+    display: flex;
+    align-items: center;
+    gap: .8rem;
+  }
+
+  .status::before {
+    content: "";
+
+    width: 8px;
+    height: 8px;
+
+    background-color: ${({ theme }) => theme.COLORS.TOMATO_300};
+
+    border-radius: 50%;
+  }
+
+  .time {
+    grid-area: time;
+    justify-self: end;
+  }
+
+  .details {
+    grid-area: details;
+  }
+
+  @media ${device.tablet} {
+    display: none;
+  }
+`;
+
+export const ContentDesktop = styled.table`
+  display: none;
+
+  width: 100%;
+  border: solid 2px ${({ theme }) => theme.COLORS.DARK_1000};
+
+  /* padding: 2.1rem 2.4rem; */
+
+  border-collapse: collapse;
+  border-radius: .8rem;
+
+  thead, tbody tr {
+    display: table;
+    table-layout: fixed;
+    width: 100%;
+  }
+
+
+  th, td {
+    border-bottom: solid 2px ${({ theme }) => theme.COLORS.DARK_1000};
+    border-right: solid 2px ${({ theme }) => theme.COLORS.DARK_1000};
+  }
+
+  thead th {
+    padding: 2.1rem 2.4rem;
+    text-align: left;
+  }
+
+  tbody td {
+    padding: 1.6rem 2.4rem;
+  }
+
+  thead th:nth-child(1),
+  tbody td:nth-child(1) {
+    width: 15rem;
+  }
+
+  thead th:nth-child(2),
+  tbody td:nth-child(2) {
+    width: 15rem;
+  }
+
+  thead th:last-child,
+  tbody td:last-child {
+    width: 15rem;
+    border-right: none;
+  }
+
+  tbody tr:last-child td {
+    border-bottom: none;
+  }
+
+  @media ${device.tablet} {
+    display: block;
   }
 `;
